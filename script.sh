7067a10ae62ff6c244c41dafbd982e324330a1a6
vipulfadtedev <vipulfadtedev@gmail.com>
2021-06-29 19:26:10 +0100

    optimised hostname fetch

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ temp=`vcgencmd measure_temp | cut -d '=' -f2 | cut -d "'" -f1`
 timestamp=`echo $(($(date +%s%N)/1000000))`
 curl --location --request POST 'http://10.0.0.7:5000/stats' \
  --header 'Content-Type: application/json' \
- --data-raw '{"host": "'$(cat /etc/hostname)'", "timestamp": '$timestamp',"counter": "temp", "value": "'$temp'"}'
+ --data-raw '{"host": "'$(hostname)'", "timestamp": '$timestamp',"counter": "temp", "value": "'$temp'"}'
 
