562f11ddc1ca75b111f6e858539ba322991fcfc0
vipulfadtedev <vipulfadtedev@gmail.com>
2021-06-29 19:13:18 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+temp=`vcgencmd measure_temp | cut -d '=' -f2 | cut -d "'" -f1`
+timestamp=`echo $(($(date +%s%N)/1000000))`
+curl --location --request POST 'http://10.0.0.7:5000/stats' \
+ --header 'Content-Type: application/json' \
+ --data-raw '{"host": "pimain", "timestamp": '$timestamp',"counter": "temp", "value": "'$temp'"}'
+
